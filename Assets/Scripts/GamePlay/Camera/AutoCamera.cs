7f4e82ef9a40c87c3b29c12d7a9211bc277c2e20
takanashi <1303170174@qq.com>
2021-06-29 23:39:02 +0800

    修改地图和摄像机
    修改地图
    尝试调整摄像机视野不超过边界（失败）

--- Assets/Scripts/GamePlay/Camera/AutoCamera.cs
+++ Assets/Scripts/GamePlay/Camera/AutoCamera.cs
@@ -9,6 +9,14 @@ public class AutoCamera : MonoBehaviour
     //修改这里数组的类型以及下面findObjectsOfType的类型来更改锁定目标的tag
     private PlayerTag[] objList;
 
+    //地图的上下左右边界
+    private float leftBoundary = -24f;
+    private float rightBoundary = 24f;
+    private float upBoundary = 10f;
+    private float downBoundary = -17f;
+    private float tmpSizeX;
+    private float tmpSizeY;
+
 
     // Update is called once per frame
     void Update()
@@ -32,7 +40,7 @@ public class AutoCamera : MonoBehaviour
 
 
         //求物体xy坐标差值
-        float xmin = objList[0].transform.position.x, xmax = objList[0].transform.position.x, ymin = objList[0].transform.position.y, ymax = objList[0].transform.position.x;
+        float xmin = objList[0].transform.position.x, xmax = objList[0].transform.position.x, ymin = objList[0].transform.position.y, ymax = objList[0].transform.position.y;
         for (int i = 0; i < objList.Length; i++)
         {
             if (objList[i].transform.position.x < xmin) { xmin = objList[i].transform.position.x; } else if (objList[i].transform.position.x > xmax) { xmax = objList[i].transform.position.x; }
@@ -45,9 +53,64 @@ public class AutoCamera : MonoBehaviour
         float test;
         if (w > l) { test = w; } else { test = l; }
 
-        //设置摄像头位置以及画幅大小
-        camera.transform.position = new Vector3(x, y, -10f);
-        camera.orthographicSize = test * 3 / 6.5f;
+        //设置摄像头位置以及画幅大小(同时不超过边界)
+        Vector3 newPos = new Vector3(x, y, -10f);
+        float newSize = test * 3 / 6.5f;
+        //float newSize2 = newSize;
+
+        /*
+        if (newPos.x - newSize * 16 / 9.0f < leftBoundary)
+        {
+            newPos = new Vector3(camera.transform.position.x, newPos.y, newPos.z);
+            newSize = camera.orthographicSize;
+        }
+        */
+
+        /*
+        if (newPos.x - newSize * 16 / 9.0f < leftBoundary)
+        {
+            if (l * 3 / 6.5f >= tmpSizeY)
+            {
+                newPos = new Vector3(camera.transform.position.x+ (l * 3 / 6.5f- camera.orthographicSize) * 16 / 9.0f, newPos.y, -10f);
+                newSize2 = l * 3 / 6.5f;
+                tmpSizeX = newSize2;
+            }
+            else if(newSize >= tmpSizeX)
+            {
+                newPos = new Vector3(camera.transform.position.x+(newSize - camera.orthographicSize) * 16 / 9.0f, newPos.y, -10f);
+                newSize2 = newSize;
+                tmpSizeY = newSize2;
+            }
+            else
+            {
+                newPos = new Vector3(camera.transform.position.x, newPos.y, -10f);
+                newSize2 = camera.orthographicSize;
+                tmpSizeX = newSize2;
+                tmpSizeY = newSize2;
+            }
+
+        }
+        else
+        {
+            tmpSizeX = camera.orthographicSize;
+            tmpSizeY = camera.orthographicSize;
+        }
+
+        if (newPos.x + newSize * 16 / 9.0f > rightBoundary)
+        {
+
+        }
+        if (newPos.y - newSize < downBoundary
+            || newPos.y + newSize > upBoundary)
+        {
+            newPos = new Vector3(newPos.x, camera.transform.position.y, -10f);
+            newSize2 = camera.orthographicSize;
+        }
+        */
+
+        camera.transform.position = newPos;
+        camera.orthographicSize = newSize;//newSize2;
+
 
     }
 }
